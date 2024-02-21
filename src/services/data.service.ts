2a66e2d782684efd1b8773975fff6f2b461c647a
talfumier <52695880+talfumier@users.noreply.github.com>
2024-02-21 17:30:31 +0100

    Update data.service.ts

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -166,11 +166,10 @@ export class DataService {
       if(!bl[2] && filter.artist["id" as keyof object]==event.performer.id)
         bl[2]=true
 
-      // if(JSON.stringify(bl).indexOf("false")===-1)
+      if(JSON.stringify(bl).indexOf("false")===-1)
         result.push(event); 
     });
     this._filteredEvents=result;
-    window.alert(this._filteredEvents.length)
     this._activeFilter=filter;
   }
   getDateFromString(date:any,format:string,output?:string):Date|number {
