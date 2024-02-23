231f6e4fbf473a49fefcfd1fdd82107db9a2b289
talfumier <henri.talfumier@gmail.com>
2024-02-23 19:12:44 +0100

    date debug

--- src/app/program/event/event.component.ts
+++ src/app/program/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component,Input, OnInit } from '@angular/core';
+import { format} from 'date-fns'
 import { ArtistEvents } from '../../../services/interfaces';
 import { removeAccents } from '../../utilities/functions/utlityFunctions';
 
@@ -13,12 +14,16 @@ export class EventComponent implements OnInit {
   private _event:ArtistEvents={}as ArtistEvents;  
 
   ngOnInit(): void {
-    this._event=this.data;  
+    this._event=this.data; 
+    console.log(format(new Date(2014, 6, 2, 15),"dd.MM.yyyy HH:mm"))
   }
 
   get event():ArtistEvents{
     return this._event;
   }  
+  getFormattedDate(date:any){
+    return format(date,"dd MMMM HH:mm")
+  }
   cleanup(type:string,location:string){
     if(type.includes("rencontre")) return "rencontre";
     return removeAccents(location);
