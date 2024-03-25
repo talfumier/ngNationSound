009ed1840e65e9cdae0828a35ff1007b91c8c1c3
talfumier <52695880+talfumier@users.noreply.github.com>
2024-03-25 09:38:51 +0100

    Update api.service.ts

--- src/services/data/init/api.service.ts
+++ src/services/data/init/api.service.ts
@@ -119,12 +119,8 @@ export class ApiService {
         break;      
       case "umap_pois": 
         if(umap_pois_url) {
-          let url=data[0].acf.umap_json.url;
-          alert(url);
-          // url=environment.production?config.api_upload_url:"/api_uploads"+"/"+url.slice(url.indexOf("uploads")+8-url.length);          
-          url=`${environment.production?config.api_upload_url:"/api_uploads"}/${url.slice(url.indexOf("uploads")+8-url.length)}`
-          alert(url);
-          // url="https://henri.mspr.dev/wp-content/uploads/2024/03/umap.json"
+          let url=data[0].acf.umap_json.url;  
+          url=`${environment.production?config.api_upload_url:"/api_uploads"}/${url.slice(url.indexOf("uploads")+8-url.length)}`;
           this.service.data[col]={url,data:{},ready:false};
         }
         else this.service.data[col]={...this.service.data[col],data,ready:true};
